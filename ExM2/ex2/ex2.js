ae7eb13192731b3fbc80cebd12b3e2843d63d894
Christian Castro <alvescastro00cca13@gmail.com>
2020-02-01 18:37:16 -0300

    Exercício 2-v2

--- ExM2/ex2/ex2.js
+++ ExM2/ex2/ex2.js
@@ -6,5 +6,18 @@ function cria(){
     let criaCaixa = document.querySelector("#caixa"); 
     criaCaixa.appendChild(div); // adicionando o elemento div dentro de outro elemento criaCaixa
 
-
+    div.setAttribute("onmouseover","getRandomColor()");//setando um novo atributo na div(caixa)...Esse atributo 
+                                                       //será usado na função getRandomColor
 }
+
+function getRandomColor() {
+
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+    }
+    document.querySelector("#box").style.backgroundColor = color;
+    
+   }
+   
